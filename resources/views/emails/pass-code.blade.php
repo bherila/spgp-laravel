af3df0200bcebe473ed3fc0941c59ae5f187b58d
bherila <ben+gh@herila.net>
2026-03-18 00:49:42 -0700

    Enhance promo code email template
    Updated promo code email template with additional instructions and clarifications.

--- resources/views/emails/pass-code.blade.php
+++ resources/views/emails/pass-code.blade.php
@@ -3,7 +3,7 @@
 
 Hello {{ $passRequest->passholder_first_name }},
 
-Your promo code for the {{ $season->pass_name }} {{ $season->pass_year }} is ready!
+Your promo code for the {{ $season->pass_name }}  is ready!
 
 **Your Promo Code:** `{{ $passRequest->promo_code }}`
 
@@ -11,6 +11,8 @@ Your promo code for the {{ $season->pass_name }} {{ $season->pass_year }} is rea
 - **Name:** {{ $passRequest->passholder_first_name }} {{ $passRequest->passholder_last_name }}
 - **Email:** {{ $passRequest->passholder_email }}
 - **Pass Type:** {{ $passRequest->pass_type }}
+
+This promo code MUST be used by the passholder above. If you need a code for a different person, then please submit a new request for that person.
 
 @if($passRequest->is_renewal)
 ## Renewal Information
@@ -20,10 +22,21 @@ Your promo code for the {{ $season->pass_name }} {{ $season->pass_year }} is rea
 @endif
 @endif
 
+# Redemption instructions
+
+1. Visit the {{ $season->pass_name }} website
+2. Add the pass (or passes) to your cart
+3. Apply the promo code(s) for each pass to your cart, you need to apply a unique promo code for each pass
+
+Remember, this code cannot be combined with other discounts, and *please* keep the details of this program confidential. 
+You can provide friends and family with the information to create their own account and request their own promo code, or request promo codes on their behalf with their permission, but don't post this deal on social media or anywhere public.
+
+Thanks for participating and please, think snow!
+
 <x-mail::button :url="config('app.url')">
 View Dashboard
 </x-mail::button>
 
 Thanks,<br>
-{{ config('app.name') }}
+Ben
 </x-mail::message>
